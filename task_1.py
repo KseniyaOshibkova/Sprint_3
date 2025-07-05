7d0909491f52b72ca43192335a1ea5bc6b35b970
KseniyaOshibkova <roksta95@gmail.com>
2025-07-05 22:12:30 +0500

    task_1

--- task_1.py
+++ task_1.py
@@ -1,5 +1,4 @@
 class OnlineSalesRegisterCollector:
-
     def __init__(self):
         self.__name_items = []
         self.__number_items = 0
@@ -25,7 +24,7 @@ class OnlineSalesRegisterCollector:
     def delete_item_from_check(self, name):
         if name not in self.__name_items:
             raise NameError('Позиция отсутствует в чеке')
-        self.__name_items.pop(name)
+        self.__name_items.remove(name)
         self.__number_items -= 1
 
     def check_amount(self):
@@ -48,10 +47,11 @@ class OnlineSalesRegisterCollector:
         for item in twenty_percent_tax:
             if item in self.__item_price:
                 total.append(self.__item_price[item])
-        nds_20 = 0
-        for to in twenty_percent_tax:
-            nds_20 += self.__item_price[to] * 0.2
-        return nds_20
+        total_sum = sum(total)
+        if len(self.__name_items) > 10:
+            total_sum *= 0.9
+
+        return total_sum * 0.2
 
     def ten_percent_tax_calculation(self):
         ten_percent_tax = []
@@ -62,10 +62,11 @@ class OnlineSalesRegisterCollector:
         for item in ten_percent_tax:
             if item in self.__item_price:
                 total.append(self.__item_price[item])
-        nds_10 = 0
-        for to in ten_percent_tax:
-            nds_10 += self.__item_price[to] * 0.1
-        return nds_10
+        total_sum = sum(total)
+        if len(self.__name_items) > 10:
+            total_sum *= 0.9
+
+        return total_sum * 0.1
 
     def total_tax(self):
         total_nds = self.twenty_percent_tax_calculation() + self.ten_percent_tax_calculation()
@@ -83,8 +84,9 @@ class OnlineSalesRegisterCollector:
 online = OnlineSalesRegisterCollector()
 online.add_item_to_cheque('кола')
 online.add_item_to_cheque('чипсы')
-online.check_amount()
-online.twenty_percent_tax_calculation()
-online.ten_percent_tax_calculation()
-online.total_tax()
-online.get_telephone_number(9466664564)
+online.add_item_to_cheque('молоко')
+print(online.check_amount())
+print(online.twenty_percent_tax_calculation())
+print(online.ten_percent_tax_calculation())
+print(online.total_tax())
+print(online.get_telephone_number(9466664564))
